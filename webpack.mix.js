36e612cf0aae2f2655d4ce69f367643c94c9e728
Diego <csjoin99@gmail.com>
2020-11-14 11:22:40 -0500

    Modificacion de webpack para incluir los archivos de js para trabajar con vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,8 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.js('resources/js/articulos.js', 'public/js');
+mix.js('resources/js/articulo-talla.js', 'public/js');
 mix.js('resources/js/cart.js', 'public/js');
 mix.js('resources/js/showarticulos.js', 'public/js');
 mix.js('resources/js/app.js', 'public/js')
