6548661bb066254078e508b46c6e1bceb81ac8c3
Diego <csjoin99@gmail.com>
2020-11-18 12:27:39 -0500

    Se modifico perPage cantidad de articulos que se muestra por pagina

--- app/Http/Controllers/ArticuloTallaController.php
+++ app/Http/Controllers/ArticuloTallaController.php
@@ -167,7 +167,7 @@ class ArticuloTallaController extends Controller
             $articulos[$i]->arrayTalla=$arrayTallas;
         }
         $page = ($request->page)?$request->page:1;
-        $perPage = 1; 
+        $perPage = 5; 
         $offset = ($page * $perPage) - $perPage;
         $paginate = new LengthAwarePaginator(
             array_slice($articulos,$offset,$perPage, true),
