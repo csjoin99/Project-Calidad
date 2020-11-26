8a61290f8cbeb95a3c80358fe13e0c0d0006d90a
Diego <csjoin99@gmail.com>
2020-11-26 13:39:16 -0500

    Se modifico las migraciones debido a un error al hacer pull y se añadio un trigger para actualizar stock

---
+++ database/migrations/2020_11_26_182026_trigger_update_stock.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class TriggerUpdateStock extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+            CREATE TRIGGER TriggerUpdateStock
+            AFTER INSERT ON detalle_ventas 
+            FOR EACH ROW
+            UPDATE articulo_tallas 
+                SET stockArticulo = stockArticulo - NEW.cantidad
+            WHERE idArticuloTalla = NEW.idArticuloTallaD;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
