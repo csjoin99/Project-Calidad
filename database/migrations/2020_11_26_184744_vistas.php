c9c4ec781e76e8eb05037be23e6a3ec6e7420491
Diego <csjoin99@gmail.com>
2020-11-26 19:27:20 -0500

    Se añadieron vistas

--- database/migrations/2020_11_26_184744_vistas.php
+++ database/migrations/2020_11_26_184744_vistas.php
@@ -15,11 +15,26 @@ class Vistas extends Migration
     public function up()
     {
         DB::statement('
-            create view recomendaciones as
+            create view vista_recomendaciones as
             select * from (SELECT *,(select COUNT(*) from articulo_tallas where 
             articulo_tallas.idArticuloS=articulos.idArticulo and articulo_tallas.estadoArticuloTalla!=0) 
             as cant from articulos where articulos.estadoArticulo!=0 order by articulos.idArticulo ASC) 
             as b where b.cant !=0 ORDER BY RAND() LIMIT 4
+        ');
+        DB::statement('
+            create view vista_articulos_cant_tallas as
+            select * from (SELECT *,(select COUNT(*) from articulo_tallas where 
+            articulo_tallas.idArticuloS=articulos.idArticulo and articulo_tallas.estadoArticuloTalla!=0) 
+            as cant from articulos where articulos.estadoArticulo!=0 order 
+            by articulos.idArticulo ASC) as b
+        ');
+        DB::statement('
+            create view vista_tallas_articulos as
+            SELECT `articulo_tallas`.`idArticuloTalla`, `articulo_tallas`.`idArticuloS`, 
+            `articulo_tallas`.`idTallaS`, `articulo_tallas`.`stockArticulo`, 
+            `tallas`.`nombreTalla` FROM `articulo_tallas` LEFT JOIN `tallas` ON 
+            `articulo_tallas`.`idTallaS` = `tallas`.`idTalla` where 
+            `articulo_tallas`.`estadoArticuloTalla`!=0
         ');
     }
 
@@ -30,6 +45,8 @@ class Vistas extends Migration
      */
     public function down()
     {
-        //
+        DB::statement('DROP VIEW IF EXISTS vista_recomendaciones');
+        DB::statement('DROP VIEW IF EXISTS vista_articulos_cant_tallas');
+        DB::statement('DROP VIEW IF EXISTS vista_tallas_articulos');
     }
 }
