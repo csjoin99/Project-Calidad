1cc837cfe34afc29ec9491128935bc5142ae0fb1
Diego <csjoin99@gmail.com>
2020-11-26 23:43:07 -0500

    Se modifico la opcion para mostrar 404 en caso de ingresar un genero que no existe para filtrar articulos

--- app/Http/Controllers/ShopStoreController.php
+++ app/Http/Controllers/ShopStoreController.php
@@ -8,33 +8,33 @@ use Illuminate\Support\Facades\DB;
 class ShopStoreController extends Controller
 {
     public function paginaPrincipal()
-    {
+    {     
         return view('shop.main')->with('Titulo', 'Bienvenido a Clothing and More');
     }
 
     public function paginaProductos($genero)
     {
-        return view('shop.products')->with(['Titulo' => 'Artículos ' . $genero, 'genderTitulo' => 'Todos', 'genero' => $genero]);
+        if ($genero == 'mujeres' || $genero == 'hombres'){
+            return view('shop.products')->with(['Titulo' => 'Artículos ' . $genero, 'genderTitulo' => 'Todos', 'genero' => $genero]);
+        }
+        abort(404);
     }
 
     public function filtroProductos($genero, Request $request)
     {
-        if ($genero == 'mujeres' || $genero == 'hombres') {
-            $generoArticulo = $genero == 'mujeres' ? 2 : 1;
-            if ($request->categoria) {
-                $articulos = DB::select("SELECT * FROM `vista_articulos_cant_tallas` WHERE cant!=0 
-                and generoArticulo=? and categoriaArticulo=?", [$generoArticulo, $request->categoria]);
-            } else {
-                $articulos = DB::select("SELECT * FROM `vista_articulos_cant_tallas` WHERE cant!=0 
-                and generoArticulo=?", [$generoArticulo]);
-            }
-            for ($i = 0; $i < count($articulos); $i++) {
-                $articulos[$i]->photoArticulo = $articulos[$i]->photoArticulo ? asset('store/' . $articulos[$i]->photoArticulo) : $articulos[$i]->photoArticulo;
-            }
-            $cantidad_articulos = count($articulos);
-            return [$articulos, $cantidad_articulos];
+        $generoArticulo = $genero == 'mujeres' ? 2 : 1;
+        if ($request->categoria) {
+            $articulos = DB::select("SELECT * FROM `vista_articulos_cant_tallas` WHERE cant!=0 
+            and generoArticulo=? and categoriaArticulo=?", [$generoArticulo, $request->categoria]);
+        } else {
+            $articulos = DB::select("SELECT * FROM `vista_articulos_cant_tallas` WHERE cant!=0 
+            and generoArticulo=?", [$generoArticulo]);
         }
-        return redirect()->route('main');
+        for ($i = 0; $i < count($articulos); $i++) {
+            $articulos[$i]->photoArticulo = $articulos[$i]->photoArticulo ? asset('store/' . $articulos[$i]->photoArticulo) : $articulos[$i]->photoArticulo;
+        }
+        $cantidad_articulos = count($articulos);
+        return [$articulos, $cantidad_articulos];
     }
     public function informacionProducto($nombreproducto)
     {
